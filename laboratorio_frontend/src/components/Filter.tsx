ef17de1ec3ea9ee9d91f6152b658eb01833c2355
Victor Felipe Dadério <daderiovictorfelipe@gmail.com>
2026-05-03 02:53:10 -0300

    recomeçando filtro

--- laboratorio_frontend/src/components/Filter.tsx
+++ laboratorio_frontend/src/components/Filter.tsx
@@ -1,20 +1,17 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 type Filtro = {
     tabela: string;
 };
 
-export function BarraBusca({ filtros, onBuscar }) {
+export function BarraBusca({ tabela }: { tabela: string }) {
     const [campoSelecionado, setCampoSelecionado] = useState('');
     const [operadorSelecionado, setOperadorSelecionado] = useState('=');
     const [valor, setValor] = useState('');
 
     const operadores = ['>', '>=', '<=', '<', '==', '='];
 
-    const handleBuscar = () => {
-        if (!campoSelecionado || !valor) return;
-        onBuscar(campoSelecionado, operadorSelecionado, valor);
-    };
+    useEffect(() => {}, [tabela]);
 
     return <div></div>;
 }
